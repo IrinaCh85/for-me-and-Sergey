934fb04d7537426b5c0baa92171ef94f25a5f508
IrinaCh85 <ichekryhova@gmail.com>
2022-12-04 17:20:28 +0300

    all even number

---
+++ task8/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+internal class Program
+{
+    static void Main(string[] args)  
+    {
+       int num = Convert.ToInt32(Console.ReadLine());
+       int res = 1;
+       while (res < num)
+       {
+        Console.WriteLine(res+1);
+        res+= 2;
+       }
+    }
+}
