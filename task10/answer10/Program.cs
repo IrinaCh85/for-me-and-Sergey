0f4ead67798afbc899b6e25ffcaeb99c76c149dc
IrinaCh85 <ichekryhova@gmail.com>
2022-12-04 19:48:38 +0300

    enter tree-digit number

---
+++ task10/answer10/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.Clear();
+        Console.WriteLine("task10");
+        Console.Write("enter tree-digit number: ");
+        int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+        string stringNumber = Convert.ToString(threeDigitNumber);
+        Console.WriteLine("second number of this number-> " + stringNumber[1]);
+    }
+}
