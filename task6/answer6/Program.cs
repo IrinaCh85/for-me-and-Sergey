409d3e05a875d6a84c5975ae7bc0e9dc4bd4fa7e
IrinaCh85 <ichekryhova@gmail.com>
2022-12-04 16:56:14 +0300

    number odd or even

---
+++ task6/answer6/Program.cs
@@ -0,0 +1,29 @@
+﻿
+    
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+        {
+            int a;
+            Console.WriteLine("enter number: ");
+            a=Convert.ToInt32(Console.ReadLine());
+
+            if (a % 2 ==0)
+            {
+                Console.WriteLine("number is odd");}
+            
+            else
+            {
+                Console.WriteLine("number is even");
+            }
+            Console.ReadKey();
+        }
+
+
+            }
+    }
+
+
+
