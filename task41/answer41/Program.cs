3e4897f54113db153a767a91a5df93fc49d24c64
IrinaCh85 <ichekryhova@gmail.com>
2022-12-22 20:47:29 +0300

    1

--- task41/answer41/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-Console.Write("enter numbers with slesh: ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = arr.Count(x => x > 0);
-Console.WriteLine($"numbers of elements > 0: {count}");
